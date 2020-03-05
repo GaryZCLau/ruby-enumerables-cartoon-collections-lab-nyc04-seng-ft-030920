279a50898526b59555db3baeb4d5aaf34ecbe67f
benign-fork-5056 <gareeeliu@gmail.com>
2020-03-05 04:47:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,8 @@ def long_planeteer_calls(call)# code an argument here
   call.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find {|cheese| cheese_types == cheese}
 end
